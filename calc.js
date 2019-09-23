456931bccf77210cee8dd2504abc607108fa02f5
Dinara <dinozavrik.dina95@gmail.com>
2019-09-23 17:01:24 +0200

    calculatrice

---
+++ calc.js
@@ -0,0 +1,13 @@
+var div = document.createElement("div");
+var para = document.createElement("p");
+var btn = document.createElement("button");
+
+
+document.body.appendChild(div);
+div.appendChild(para);
+div.appendChild(btn);
+
+
+for(let i=0;i<13; i++) {
+    
+} 
