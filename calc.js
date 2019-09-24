fdf64b1c028dc7e07db9b8457927e60d43ff220a
dadurand <ha.halo@live.fr>
2019-09-24 10:12:27 +0200

    avancement

--- calc.js
+++ calc.js
@@ -1,13 +1,39 @@
 var div = document.createElement("div");
 var para = document.createElement("p");
-var btn = document.createElement("button");
 
 
 document.body.appendChild(div);
 div.appendChild(para);
-div.appendChild(btn);
+var text = document.getElementsByTagName("p")[0];
+charlist = [1, 2, 3, "+", 4, 5, 6, "-", 7, 8, 9, "*", 0, "=", "/", "reset"];
+j = 0;
+
+btn = this.addEventListener("click", function () {
+    text.innerHTML(charlist[i]);
+})
+
+for (let i = 0; i < 16; i++) {
+    var btn = document.createElement("button");
+    btn.append(charlist[j]);
+    div.appendChild(btn);
+
+    j++;
+
+}
+
+var buttonEgal = document.getElementsByTagName("button")[13];
+
+buttonEgal.id = "egal";
+
+var buttonReset = document.getElementsByTagName("button")[15];
+
+buttonReset.id = "reset";
 
 
-for(let i=0;i<13; i++) {
-    
-} 
+
+
+// var egal = document.createElement("button");
+// egal.id="egal";
+// div.appendChild(egal);
+
+// var btn = document.createElement("button");
