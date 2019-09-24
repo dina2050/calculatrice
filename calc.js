8f6b3db50450fe3e6fed3e87167640f15b8a1abd
dadurand <ha.halo@live.fr>
2019-09-24 12:08:23 +0200

    fin

--- calc.js
+++ calc.js
@@ -4,21 +4,26 @@ var para = document.createElement("p");
 
 document.body.appendChild(div);
 div.appendChild(para);
-// var text = document.getElementsByTagName("p")[0];
 charlist = ["1", "2", "3", "+", "4", "5", "6", "-", "7", "8", "9", "*", "0", "=", "/", "reset"];
 j = 0;
 
 for (let i = 0; i < 16; i++) {
     var btn = document.createElement("button");
-    btn.append(charlist[j]);
+    btn.innerHTML=charlist[i];
     div.appendChild(btn);
-    btn.value=charlist[j];
-    
-    btn.addEventListener("click", function () {
-        para.innerHTML+=this.value;
-    });
-    j++;
 
+    btn.addEventListener("click", function(){
+        if (this.innerHTML == "="){
+            para.innerHTML = eval(para.innerHTML)
+        }
+        else if (this.value == "reset"){
+            para.innerHTML= ""; 
+        }
+        else {
+            this.innerHTML=charlist[i];
+            para.innerHTML += this.innerHTML;
+        }
+    })
 }
 
 var buttonEgal = document.getElementsByTagName("button")[13];
@@ -28,7 +33,3 @@ buttonEgal.id = "egal";
 var buttonReset = document.getElementsByTagName("button")[15];
 
 buttonReset.id = "reset";
-
-
-
-// para.innerHTML+=parseInt(this.value);
