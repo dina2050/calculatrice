2829befb256bac5a36da66525afa3ba2e15d14b5
Dinara <dinozavrik.dina95@gmail.com>
2019-09-24 11:17:24 +0200

    calculatrice

--- calc.js
+++ calc.js
@@ -4,19 +4,19 @@ var para = document.createElement("p");
 
 document.body.appendChild(div);
 div.appendChild(para);
-var text = document.getElementsByTagName("p")[0];
-charlist = [1, 2, 3, "+", 4, 5, 6, "-", 7, 8, 9, "*", 0, "=", "/", "reset"];
+// var text = document.getElementsByTagName("p")[0];
+charlist = ["1", "2", "3", "+", "4", "5", "6", "-", "7", "8", "9", "*", "0", "=", "/", "reset"];
 j = 0;
-
-btn = this.addEventListener("click", function () {
-    text.innerHTML(charlist[i]);
-})
 
 for (let i = 0; i < 16; i++) {
     var btn = document.createElement("button");
     btn.append(charlist[j]);
     div.appendChild(btn);
-
+    btn.value=charlist[j];
+    
+    btn.addEventListener("click", function () {
+        para.innerHTML+=this.value;
+    });
     j++;
 
 }
@@ -31,9 +31,4 @@ buttonReset.id = "reset";
 
 
 
-
-// var egal = document.createElement("button");
-// egal.id="egal";
-// div.appendChild(egal);
-
-// var btn = document.createElement("button");
+// para.innerHTML+=parseInt(this.value);
