17e0c61b14a685a2f513b8540038660affc6c97c
dadurand <ha.halo@live.fr>
2019-09-27 14:50:33 +0200

    correction symboles

--- calc.js
+++ calc.js
@@ -5,7 +5,7 @@ var para = document.createElement("p");
 document.body.appendChild(div);
 div.appendChild(para);
 charlist = ["1", "2", "3", "+", "4", "5", "6", "-", "7", "8", "9", "*", "0", "=", "/", "reset"];
-oplist = ["+", "-", "=", "/"];
+opList = ["+", "-", "*", "/"]
 
 for (let i = 0; i < 16; i++) {
     var btn = document.createElement("button");
@@ -19,8 +19,8 @@ for (let i = 0; i < 16; i++) {
         else if (this.innerHTML == "reset"){
             para.innerHTML= ""; 
         }
-        else if (this.innerHTML=="+" || this.innerHTML=="-" || this.innerHTML=="*" || this.innerHTML=="/"){
-            para.innerHTML += "k";
+        else if (opList.includes(this.innerHTML) && opList.includes(para.innerHTML[para.innerHTML.length-1])){
+            para.innerHTML += ""
         }
         // else if (this.innerHTML=="+" || this.innerHTML=="-" || this.innerHTML=="*" || this.innerHTML=="/"){
         //     // if (para.innerHTML[para.innerHTML.length-1]=="+" || para.innerHTML[para.innerHTML.length-1]=="-" || para.innerHTML[para.innerHTML.length-1]=="*" || para.innerHTML[para.innerHTML.length-1]=="/"){
